07d68ebf734d09697066c3e99b28a15ca7f5c1d0
velopace <velopace@outlook.com>
2023-01-19 16:24:04 -0600

    sns subscribe from lambda

---
+++ 25-SNS subscribe from lambda/index.mjs
@@ -0,0 +1,6 @@
+export const handler = async (event) => {
+  console.log('event:', JSON.stringify(event, undefined, 2));
+
+  var message = event.Records[0].Sns.Message;
+  console.log('Message received from SNS:', message);
+};
