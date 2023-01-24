a19bc1627471c66ebc87213bb4e16da1b9a5d535
velopace <velopace@outlook.com>
2023-01-24 04:02:03 -0600

    sqs polling

---
+++ 27-SQS polling from Lambda/index.mjs
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+  console.log('event: ', JSON.stringify(event, undefined, 2));
+
+  event.Records.forEach((record) => {
+    const { body } = record;
+    console.log(body);
+  });
+  return {};
+};
